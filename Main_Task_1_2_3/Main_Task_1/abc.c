93b4038920001c3aa42796cc4c900acc62ea35e0
wsl_ubuntu20.04 <jd.developer2020@gmail.com>
2021-04-17 09:46:09 +0800

    update task1

--- Main_Task_1_2_3/Main_Task_1/abc.c
+++ Main_Task_1_2_3/Main_Task_1/abc.c
@@ -55,3 +55,119 @@ void Timer1(void)
 
 	PR1 = 368.5; //100us
 	
+	
+	IPC0bits.T1IP = 1;// Set Timer1 Interrupt Priority Level
+	IFS0bits.T1IF = 0;// Clear Timer1 Interrupt Flag
+	IEC0bits.T1IE = 1;// Enable Timer1 interrupt
+	
+	T1CONbits.TON = 1;
+}
+
+void __attribute__((__interrupt__, no_auto_psv)) _T1Interrupt(void)
+{
+	/* Interrupt Service Routine code goes here */
+	//if(LED1) LED1=0; else LED1=1;
+	
+	count_us++;
+	count_ms++;
+	count_Led++;
+	count_us_c++;
+	count_us_s++;
+	/*
+	int i;
+	for(i = 0; i < 2000; i++)
+	{
+		if(LED2) LED2=0; else LED2=1;
+	}*/
+	if (count_Led == 400){
+		count_Led = 0;
+		if(LED2) LED2=0; else LED2=1;
+		if(L1) L1=0; else L1=1;
+	}	
+	
+	IFS0bits.T1IF = 0; // Clear Timer1 Interrupt Flag
+}
+
+void delay_500us(void)
+{
+	while(count_us < 5){
+	}
+	count_us = 0;
+}
+
+void delay_200ms(void)
+{
+	while(count_us < 2000){
+	}
+	count_ms = 0;	
+}		
+
+void Task(void)
+{
+	/*
+	data_submit(0b00000000);
+	data_submit(0b10000000);
+	data_submit(0b01000000);
+	data_submit(0b00100000);
+	data_submit(0b00010000);
+	data_submit(0b00001000);
+	data_submit(0b00000100);
+	data_submit(0b00000010);
+	data_submit(0b00000001);
+	data_submit(0xFF);  */   
+	
+	switch(count_ms)
+	{
+		case 2000:
+			data_submit(0b00000000);
+			break;
+		case 4000:
+			data_submit(0b10000000);
+			break;
+		case 6000:
+			data_submit(0b01000000);
+			break;			
+		case 8000:
+			data_submit(0b00100000);
+			break;
+		case 10000:
+			data_submit(0b00010000);
+			break;
+		case 12000:
+			data_submit(0b00001000);
+			break;
+		case 14000:
+			data_submit(0b00000100);
+			break;
+		case 16000:
+			data_submit(0b00000010);
+			break;
+		case 18000:
+			data_submit(0b00000001);
+			break;
+		case 20000:
+			data_submit(0xFF);	
+			count_ms = 0;
+			break;
+	}																						
+}
+
+int main()
+{
+	TRIS_init();
+	Timer1();
+	while(1){
+		if(count_ms == 2000)
+		{
+			count_ms = 0;
+			if(LED1) LED1 = 0;
+			else LED1 = 1;
+		}	
+		Task();
+	
+		L1 = 1;
+		L2 = 1;
+		L3 = 0;
+		L4 = 0;
+	}
+}	
